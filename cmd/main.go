66cff8dccaeabb9d81119fc23f374263cebff335
Joe Lombrozo <joe@djeebus.net>
2024-12-07 00:15:33 -0500

    more progress
    Signed-off-by: Joe Lombrozo <joe@djeebus.net>

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,19 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 
 	"github.com/pkg/errors"
 )
 
-type installers struct {
-	OS, Path string
+type file struct {
+	ManualURL string `json:"manualUrl"`
+	Name      string `json:"name"`
+	Version   string `json:"version"`
+	Date      string `json:"date"`
+	Size      string `json:"size"`
 }
 
 type gameData struct {
@@ -34,13 +39,12 @@ func Cmd() error {
 	}
 
 	for _, gameID := range gameIDs {
-		data, err := getGameData(gameID, cookies, client)
+		files, err := getDownloads(gameID, cookies, client)
 		if err != nil {
 			return errors.Wrapf(err, "failed to get game data for %s", gameID)
 		}
 
-		_, _, err = findLatestDownload("English", "windows", data)
-		return err
+		fmt.Printf("Found %d files\n", len(files))
 	}
 
 	return nil
@@ -57,27 +61,68 @@ func findLatestDownload(language, platform string, data gameData) (string, []any
 	return "", nil, nil
 }
 
-func getGameData(gameID string, cookies map[string]string, client http.Client) (gameData, error) {
+func getDownloads(gameID string, cookies map[string]string, client http.Client) ([]file, error) {
 	// GET https://www.gog.com/account/gameDetails/$id.json
 	url := fmt.Sprintf("https://www.gog.com/account/gameDetails/%s.json", gameID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return gameData{}, errors.Wrap(err, "failed to create second request")
+		return nil, errors.Wrap(err, "failed to create second request")
 	}
 	addCookies(req, cookies)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return gameData{}, errors.Wrap(err, "failed to fetch second request")
+		return nil, errors.Wrap(err, "failed to fetch second request")
 	}
 	defer resp.Body.Close()
 
-	var data gameData
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		return gameData{}, errors.Wrap(err, "failed to decode second request")
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read second request")
 	}
 
-	return data, nil
+	if err = os.WriteFile(fmt.Sprintf("%s.json", gameID), body, 0644); err != nil {
+		return nil, errors.Wrap(err, "failed to write second request")
+	}
+
+	var data gameData
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, errors.Wrap(err, "failed to decode second request")
+	}
+
+	for _, download := range data.Downloads {
+		if len(download) != 2 {
+			return nil, errors.New("downloads is not a 2-length tuple")
+		}
+
+		if download[0] != "English" {
+			continue
+		}
+
+		oses, ok := download[1].(map[string]any)
+		if !ok {
+			return nil, errors.New("downloads[1] is not a map[string]any")
+		}
+
+		windows, ok := oses["windows"]
+		if !ok {
+			return nil, errors.New("downloads[1] windows is not found")
+		}
+
+		data, err := json.Marshal(windows)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to encode windows files")
+		}
+
+		var files []file
+		if err = json.Unmarshal(data, &files); err != nil {
+			return nil, errors.Wrap(err, "failed to decode windows files")
+		}
+
+		return files, nil
+	}
+
+	return nil, errors.New("failed to find a single download")
 }
 
 func getGameIDs(cookies map[string]string, client http.Client) ([]string, error) {
