c782b83e7d5abfe173efcbee857a0a52b29f5f53
Joe Lombrozo <joe@djeebus.net>
2024-12-07 12:06:07 -0500

    finish implementing simple archiver
    Signed-off-by: Joe Lombrozo <joe@djeebus.net>

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -27,69 +32,154 @@ type gameData struct {
 func Cmd() error {
 	var client http.Client
 
-	cookies := map[string]string{
-		"gog-al": os.Getenv("AUTH_GOG_AL"),
-		"gog_lc": os.Getenv("AUTH_GOG_LC"),
-		"gog_us": os.Getenv("AUTH_GOG_US"),
+	var rootPath string
+	var filterGameID string
+
+	args := os.Args
+	switch len(args) {
+	case 2:
+		rootPath = args[1]
+	case 3:
+		rootPath = args[1]
+		filterGameID = args[2]
+	default:
+		fmt.Printf("Usage: %s PATH [GAMEID]\n", args[0])
+		os.Exit(1)
 	}
 
-	gameIDs, err := getGameIDs(cookies, client)
+	gogURL, err := url.Parse("https://www.gog.com")
+	if err != nil {
+		panic(err)
+	}
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+	jar.SetCookies(gogURL, []*http.Cookie{
+		{Name: "gog-al", Value: os.Getenv("AUTH_GOG_AL"), Domain: "gog.com"},
+		{Name: "gog_lc", Value: os.Getenv("AUTH_GOG_LC"), Domain: "gog.com"},
+		{Name: "gog_us", Value: os.Getenv("AUTH_GOG_US"), Domain: "gog.com"},
+	})
+	client.Jar = jar
+
+	gameIDs, err := getGameIDs(client)
 	if err != nil {
 		return err
 	}
 
 	for _, gameID := range gameIDs {
-		files, err := getDownloads(gameID, cookies, client)
+		if filterGameID != "" && gameID != filterGameID {
+			continue
+		}
+
+		gameData, err := getGameData(gameID, &client)
 		if err != nil {
 			return errors.Wrapf(err, "failed to get game data for %s", gameID)
 		}
 
-		fmt.Printf("Found %d files\n", len(files))
+		files, err := getDownloads(gameData)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get game data for %s", gameID)
+		}
+
+		filename := createFilename(gameData, files[0])
+		filename = filepath.Join(rootPath, filename)
+
+		if _, err := os.Stat(filename); err == nil {
+			fmt.Printf("%q already downloaded, skipping\n", filename)
+		}
+
+		fmt.Printf("Found %d files, downloading\n", len(files))
+		if err = createGamePackage(filename, files, &client); err != nil {
+			return errors.Wrapf(err, "failed to create game package for %s", gameID)
+		}
 	}
 
 	return nil
 }
 
-func findLatestDownload(language, platform string, data gameData) (string, []any, error) {
-	for _, installers := range data.Downloads {
+func createGamePackage(zipFilename string, files []file, client *http.Client) error {
+	file, err := os.Create(zipFilename)
+	if err != nil {
+		return errors.Wrap(err, "failed to create file")
+	}
+	defer file.Close()
 
-		if len(installers) != 2 {
-			println("installers is not a 2-length tuple")
+	zipWriter := zip.NewWriter(file)
+	defer zipWriter.Close()
+
+	for _, f := range files {
+		fileUrl, err := url.JoinPath("https://www.gog.com", f.ManualURL)
+		if err != nil {
+			return errors.Wrap(err, "failed to make file url")
 		}
 
+		req, err := http.NewRequest("GET", fileUrl, nil)
+		if err != nil {
+			return errors.Wrap(err, "failed to create request")
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return errors.Wrap(err, "failed to download file")
+		}
+		defer res.Body.Close()
+
+		_, filename := filepath.Split(res.Request.URL.Path)
+
+		w, err := zipWriter.Create(filename)
+		if err != nil {
+			return errors.Wrap(err, "failed to create zip file")
+		}
+
+		if _, err = io.Copy(w, res.Body); err != nil {
+			return errors.Wrap(err, "failed to copy file")
+		}
 	}
-	return "", nil, nil
+
+	return nil
 }
 
-func getDownloads(gameID string, cookies map[string]string, client http.Client) ([]file, error) {
+func createFilename(data gameData, file file) string {
+	release := time.Unix(data.ReleaseTimestamp, 0)
+
+	return fmt.Sprintf("%s (%s) (%d).zip", data.Title, file.Version, release.Year())
+}
+
+func getGameData(gameID string, client *http.Client) (gameData, error) {
+	var data gameData
+
 	// GET https://www.gog.com/account/gameDetails/$id.json
 	url := fmt.Sprintf("https://www.gog.com/account/gameDetails/%s.json", gameID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to create second request")
+		return data, errors.Wrap(err, "failed to create second request")
 	}
-	addCookies(req, cookies)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to fetch second request")
+		return data, errors.Wrap(err, "failed to fetch second request")
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to read second request")
+		return data, errors.Wrap(err, "failed to read second request")
 	}
 
 	if err = os.WriteFile(fmt.Sprintf("%s.json", gameID), body, 0644); err != nil {
-		return nil, errors.Wrap(err, "failed to write second request")
+		return data, errors.Wrap(err, "failed to write second request")
 	}
 
-	var data gameData
 	if err := json.Unmarshal(body, &data); err != nil {
-		return nil, errors.Wrap(err, "failed to decode second request")
+		return data, errors.Wrap(err, "failed to decode second request")
 	}
 
+	return data, nil
+}
+
+func getDownloads(data gameData) ([]file, error) {
 	for _, download := range data.Downloads {
 		if len(download) != 2 {
 			return nil, errors.New("downloads is not a 2-length tuple")
@@ -125,12 +215,11 @@ func getDownloads(gameID string, cookies map[string]string, client http.Client) 
 	return nil, errors.New("failed to find a single download")
 }
 
-func getGameIDs(cookies map[string]string, client http.Client) ([]string, error) {
+func getGameIDs(client http.Client) ([]string, error) {
 	req, err := http.NewRequest("GET", "https://menu.gog.com/v1/account/licences", nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create first request")
 	}
-	addCookies(req, cookies)
 
 	resp, err := client.Do(req)
 	if err != nil {
